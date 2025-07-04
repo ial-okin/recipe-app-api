1aea598dc55f57e859a94472b3a038f917647610
Nikolai Kalistratov <nikolai@neuronsinc.com>
2025-07-04 07:31:16 +0200

    Adding tests

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x: int, y: int):
+    """Add x and y and return result."""
+    return x + y
+
+
+def subtract(x: int, y: int):
+    """Subtract x from y and return result."""
+    return y - x
