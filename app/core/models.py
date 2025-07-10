3e8a155b9cf582a248c79d1ef9b07e92d6939fa9
Nikolai Kalistratov <nikolai@neuronsinc.com>
2025-07-10 19:40:42 +0200

    Creating Recipe Model

--- app/core/models.py
+++ app/core/models.py
@@ -2,8 +2,7 @@
 Database models.
 """
 
-from typing import Any, Optional
-
+from django.conf import settings
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
@@ -11,18 +10,17 @@ from django.db import models
 class UserManager(BaseUserManager):
     """Manager for users."""
 
-    def create_user(self, email: str, password: Optional[str] = None, **extra_fields: Any):
+    def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
         if not email:
-            raise ValueError("Users must have an email address")
-
+            raise ValueError("User must have an email address.")
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(self, email: str, password: str):
+    def create_superuser(self, email, password):
         """Create and return a new superuser."""
         user = self.create_user(email, password)
         user.is_staff = True
@@ -43,3 +41,20 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
+
+
+class Recipe(models.Model):
+    """Recipe object."""
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
