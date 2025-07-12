32f3eb4e85cea20ce28390b16b2d9d0f3ec26a95
Nikolai Kalistratov <nikolai@neuronsinc.com>
2025-07-12 11:42:36 +0200

    Creating "Tag" model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,21 +8,25 @@ from core import models
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
+user_email = "test@example.com"
+user_password = "testpass123"
+
+
+def create_user(email: str, password: str) -> models.User:
+    """Create a return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
 
 class ModelTests(TestCase):
     """Test models."""
 
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
-        email = "test@example.com"
-        password = "testpass123"
-        user = get_user_model().objects.create_user(
-            email=email,
-            password=password,
-        )
 
-        self.assertEqual(user.email, email)
-        self.assertTrue(user.check_password(password))
+        user = create_user(user_email, user_password)
+
+        self.assertEqual(user.email, user_email)
+        self.assertTrue(user.check_password(user_password))
 
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users."""
@@ -33,27 +37,24 @@ class ModelTests(TestCase):
             ["test4@example.COM", "test4@example.com"],
         ]
         for email, expected in sample_emails:
-            user = get_user_model().objects.create_user(email, "sample123")
+            user = create_user(email, "sample123")
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
         """Test that creating a user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user("", "test123")
+            create_user("", "test123")
 
     def test_create_superuser(self):
         """Test creating a superuser."""
-        user = get_user_model().objects.create_superuser(
-            "test@example.com",
-            "test123",
-        )
+        user = get_user_model().objects.create_superuser(user_email, user_password)
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
 
     def test_create_recipe(self):
         """Test creating a recipe is successful."""
-        user = get_user_model().objects.create_user(
+        user = create_user(
             "test@example.com",
             "testpass123",
         )
@@ -66,3 +67,11 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful."""
+        user = create_user(user_email, user_password)
+        # TODO: move to a helper "seed" function if used in multiple tests
+        tag = models.Tag.objects.create(user=user, name="Tag1")
+
+        self.assertEqual(str(tag), tag.name)
